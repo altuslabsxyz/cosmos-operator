bb8197bea7ca0f987cc11cfa960f02e2950e8209
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2025-11-12 20:17:21 +0900

    fix: use podTemplate image as default

--- cmd/versioncheck.go
+++ cmd/versioncheck.go
@@ -166,7 +166,7 @@ func checkVersion(
 		return err
 	}
 
-	var image string
+	image := crd.Spec.PodTemplate.Image
 	for _, v := range crd.Spec.ChainSpec.Versions {
 		if uint64(height) < v.UpgradeHeight {
 			break
